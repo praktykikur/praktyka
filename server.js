a836b53e60580b0051a613e270cd3006e7058ce9
gssgg69 <szymon.nocun@zs6sobieski.edu.pl>
2024-04-05 11:25:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ module.exports = {
             //let sql = "SELECT * FROM logowanie";
             connection.query("INSERT INTO praktyki SET ?", { imie:req.imie, nazwisko:req.nazwisko, kod_pocztowy:req.kodpocztowy, miejscowosc:req.miasto, wojewodztwo:req.woj, powiat:req.powiat, gmina:req.gmina, ulica:req.ulica, nr_domu:req.nr_domu, nr_lokalu:req.nr_lokalu, telefon:req.telefon, email:req.email, pesel:req.pesel, nr_konta:req.rachunek, miejsce_stazu:req.miejsce_praktyk, klasa:req.klasa, kierunek:req.kierunek_zaw}, function(err, res){
                 if (err){
-                    console.log("NIE OK", err); //w grze the sims4
+                    console.log("NIE OK", err); //w grze the sims4 gddggdgd
                     return cb(err);
                 }
                 return cb("ok")
